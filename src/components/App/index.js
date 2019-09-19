56caa0557ec9bfe28f2ead23b91d5b54d603415b
Maciej Kurek <maciejk77@gmail.com>
2019-09-19 20:17:39 +0100

    grid refactored, real data plug in

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,41 +1,13 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { Fragment } from 'react';
 import CardGrid from '../CardGrid';
 import './styles.scss';
 
-// const App = () => <CardGrid />;
-const App = () => {
-  let [isExtended, extendCard] = useState(Array(15).fill(false));
-  let card = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-
-  const toggleCard = e => {
-    const cards = isExtended.slice();
-    const val = e.target.getAttribute('value');
-    cards[val] = !cards[val];
-    extendCard(cards);
-  };
-
-  return (
-    <div className="wrapper">
-      {card.map(c => {
-        return (
-          <div
-            style={
-              isExtended[c]
-                ? { gridRowEnd: 'span 2', gridColumnEnd: 'span 2' }
-                : null
-            }
-            key={c}
-            onClick={toggleCard}
-            value={c}
-          >
-            <div>Title {c}</div>
-            <div>Content {c}</div>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+const App = () => (
+  <Fragment>
+    {/* <Header /> */}
+    <CardGrid />
+    {/* <Footer /> */}
+  </Fragment>
+);
 
 export default App;
--- src/components/CardGrid/index.js
+++ src/components/CardGrid/index.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { stories } from '../../data/data.json';
-import Card from '../Card';
 import './styles.scss';
 
 const CardGrid = () => {
-  return stories.map(story => (
-    <div className="card-grid" key={story.id}>
-      <Card story={story} />
+  let [isExtendedMap, extendCard] = useState(
+    Array(stories.map(story => story.id).length).fill(false)
+  );
+
+  const toggleCard = e => {
+    const cards = isExtendedMap.slice();
+    const val = e.target.getAttribute('value');
+    cards[val] = !cards[val];
+    extendCard(cards);
+  };
+
+  // refactor to styled component ExpandableCard?
+  const isExtendedLogic = id =>
+    isExtendedMap[id]
+      ? { gridRowEnd: 'span 2', gridColumnEnd: 'span 2' }
+      : null;
+
+  return (
+    <div className="card-grid">
+      {stories.map(({ id, title, text }) => {
+        return (
+          <div
+            className="card-grid__card"
+            key={id}
+            onClick={toggleCard}
+            style={isExtendedLogic(id)}
+            value={id}
+          >
+            <div>Title {title}</div>
+            <div>Content {text}</div>
+          </div>
+        );
+      })}
     </div>
-  ));
+  );
 };
 
 export default CardGrid;
