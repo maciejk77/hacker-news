21dd82b9ab5b8e97b197fd074f4b0edfe24e5a8e
Maciej Kurek <maciejk77@gmail.com>
2019-09-19 15:15:17 +0100

    grid refactored to map render

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,72 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import CardGrid from '../CardGrid';
 import './styles.scss';
 
 // const App = () => <CardGrid />;
+const App = () => {
+  const [isCardExtended, extendCard] = useState(false);
+  const card = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-const App = () => (
-  <div className="wrapper">
-    <div>
-      <div>Title 1</div>
-      <div>Content 1</div>
+  const toggleCard = e => {
+    extendCard(!isCardExtended);
+  };
+
+  return (
+    <div className="wrapper">
+      {card.map(c => {
+        return (
+          <div
+            style={isCardExtended ? { width: '200px', height: '200px' } : null}
+            key={c}
+            onClick={toggleCard}
+            value={c}
+          >
+            <div>Title {c}</div>
+            <div>Content {c}</div>
+          </div>
+        );
+      })}
     </div>
-    <div className="box2">
-      <div>Title 2</div>
-      <div>Content 2</div>
-    </div>
-    <div>
-      <div>Title 3</div>
-      <div>Content 3</div>
-    </div>
-    <div>
-      <div>Title 4</div>
-      <div>Content 4</div>
-    </div>
-    <div>
-      <div>Title 5</div>
-      <div>Content 5</div>
-    </div>
-    <div>
-      <div>Title 6</div>
-      <div>Content 6</div>
-    </div>
-    <div>
-      <div>Title 7</div>
-      <div>Content 7</div>
-    </div>
-    <div className="box8">
-      <div>Title 8</div>
-      <div>Content 8</div>
-    </div>
-    <div>
-      <div>Title 9</div>
-      <div>Content 9</div>
-    </div>
-    <div>
-      <div>Title 10</div>
-      <div>Content 10</div>
-    </div>
-    <div>
-      <div>Title 11</div>
-      <div>Content 11</div>
-    </div>
-    <div>
-      <div>Title 12</div>
-      <div>Content 12</div>
-    </div>
-    <div>
-      <div>Title 13</div>
-      <div>Content 13</div>
-    </div>
-    <div>
-      <div>Title 14</div>
-      <div>Content 14</div>
-    </div>
-    <div>
-      <div>Title 15</div>
-      <div>Content 15</div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default App;
