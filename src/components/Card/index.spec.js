09b75b8d1762ea05b5c3b2fe403e1c7b113206c0
Maciej Kurek <maciejk77@gmail.com>
2019-10-05 15:07:15 +0100

    Card component in TDD, tests written

--- src/components/Card/index.spec.js
+++ src/components/Card/index.spec.js
@@ -7,4 +7,23 @@ describe('<Card />', () => {
     const wrapper = shallow(<Card />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  // no need if speacific node is included in markup
+  // this may change, better yet to test whether props were received
+
+  it('should receive props id,score,title,kids', () => {});
+
+  describe('should expand/collapse a card', () => {
+    it('starts with a collapsed card', () => {});
+    it('expands a card on first/odd click', () => {});
+    it('collapses a card on second/even click', () => {});
+  });
+
+  describe('should set prev/next comment on click', () => {
+    it('should start with first comment', () => {});
+    it('should set previous comment', () => {});
+    it('should set next comment', () => {});
+    it('should set first comment when received click on last', () => {});
+    it('should set last comment when received click on first', () => {});
+  });
 });
